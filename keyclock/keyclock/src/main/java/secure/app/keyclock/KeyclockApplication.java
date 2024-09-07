9bf7e050912c53b24d1ea12914c669f21699254b
DhouhaOun <Dhouha.ouni93@gmail.com>
2024-09-07 21:42:46 +0200

    desactiver spring security

--- keyclock/keyclock/src/main/java/secure/app/keyclock/KeyclockApplication.java
+++ keyclock/keyclock/src/main/java/secure/app/keyclock/KeyclockApplication.java
@@ -1,7 +1,11 @@
 package secure.app.keyclock;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import secure.app.keyclock.entities.Product;
+import secure.app.keyclock.repository.ProductRepository;
 
 @SpringBootApplication
 public class KeyclockApplication {
@@ -10,4 +14,15 @@ public class KeyclockApplication {
 		SpringApplication.run(KeyclockApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(ProductRepository productRepository){
+		return args -> {
+			productRepository.save(Product.builder().id("P01").name("Computer").price(2300).quantity(5)
+					.build());
+			productRepository.save(Product.builder().id("P02").name("Printer").price(1200).quantity(10)
+					.build());
+			productRepository.save(Product.builder().id("P03").name("Smart Phone").price(4200).quantity(34)
+					.build());
+		};
+	}
 }
